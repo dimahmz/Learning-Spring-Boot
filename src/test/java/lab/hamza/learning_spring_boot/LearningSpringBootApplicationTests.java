a38f2a50f2b9984c59ff988c110344c4cb203bf2
dimahmz <essaadihamza4@outlook.com>
2024-11-23 21:30:47 +0100

    init project

---
+++ src/test/java/lab/hamza/learning_spring_boot/LearningSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.hamza.learning_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
