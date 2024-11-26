8bb81f2038aacb9a54b9c0ff645601d27637c993
dimahmz <essaadihamza4@outlook.com>
2024-11-26 11:44:13 +0100

    allow all the origins

--- src/main/java/lab/hamza/learning_spring_boot/controller/ContentController.java
+++ src/main/java/lab/hamza/learning_spring_boot/controller/ContentController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/content")
+@CrossOrigin
 public class ContentController {
     private final ContentCollectionRepository repository;
 
